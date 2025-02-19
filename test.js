87f60c07437435b2ef4d0b2b1fad78b5d62f498b
quotingg. <112746878+quotingg@users.noreply.github.com>
2025-02-19 16:03:12 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 const exp = require("express");
-const prox = require("express-http-proxy");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const path = require("node:path");
 const app = exp();
@@ -13,26 +12,25 @@ App.use("/play", Express.static(Path.join(__dirname, "play")));
 App.use("/3", Express.static(Path.join(__dirname, "3")));
 
 
-/*const targ = "https://doctoraux.com"
+const targ = "https://doctoraux.com"
 const prox = createProxyMiddleware({
     target: targ,
     changeOrigin: true,
     secure: true,
     logLevel: "debug",
     router: function (req) {
-        if (req.headers.host === new URL(targ).host) {
-            req.headers['X-Forwarded-For'] = '';
-            req.headers['X-Real-IP'] = '';
-            req.headers['Via'] = '';
-        }
+        delete req.headers['x-forwarded-for'];
+        delete req.headers['x-forwarded-proto'];
+        delete req.headers['x-real-ip'];
+        console.log(req.headers);
         return targ;
     }
-})*/
+})
 
 app.get("/", (_Request, Response) => {
     Response.sendFile(path.join(__dirname, "apps/uncube/10005", "now.html"));
 })
-app.use("/", prox('https://educationbluesky.com'));
+app.use("/", prox);
 app.listen(5000, () => {
     console.log("run");
 })
